dad52a699931c45b95db5d91856591bcd3249e77
4890camilo <camilinchi.1235@gmail.com>
2025-10-20 16:21:15 -0500

    Aplicar .gitignore y limpiar archivos innecesarios

--- institucion/institucion/settings.py
+++ institucion/institucion/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'usuarios',
+    'academico',
+    'financiero',
+    'nomina',
+    'reportes',
 ]
 
 MIDDLEWARE = [
